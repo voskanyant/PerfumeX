4a01760afd3976f77985bc0ef61ad59422ab7c1c
Tigran Voskanyan <C:\Users\tigra\AppData\Roaming\The Bat!>
2026-04-08 11:50:10 +0300

    Serve production static assets with WhiteNoise

--- perfumex/settings.py
+++ perfumex/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'prices.middleware.ForceMoscowTimezoneMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,6 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
