e4046e4fc0496bb8678ff18380d711f016a48990
Tigran <tigranvoskanyan1993@gmail.com>
2026-04-30 09:35:09 +0300

    Merge assistant linking migration branches

---
+++ assistant_linking/migrations/0031_merge_0024_seed_woodbox_variant_type_0030_seed_salvador_dali_brand_aliases.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("assistant_linking", "0024_seed_woodbox_variant_type"),
+        ("assistant_linking", "0030_seed_salvador_dali_brand_aliases"),
+    ]
+
+    operations = []
